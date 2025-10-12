ba73d82c2d4c6a5ba0bf623809a73c8cca9c8c43
Yusong <yusonglai64@gmail.com>
2025-10-12 19:54:57 +0800

    chore(deps): add "awscli2" package in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A Nix-flake-based development environment";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+
   outputs =
     inputs:
     let
@@ -13,21 +15,30 @@
       forEachSupportedSystem =
         f:
         inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system: f { pkgs = import inputs.nixpkgs { inherit system; }; }
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+            pkgs-stable = import inputs.nixpkgs-stable { inherit system; };
+          }
         );
     in
     {
       devShells = forEachSupportedSystem (
-        { pkgs }:
+        { pkgs, pkgs-stable }:
         {
           default = pkgs.mkShell {
             # The Nix packages installed in the dev environment.
-            packages = with pkgs; [
-              just # just a command runner
-              cocogitto # conventional commit toolkit
-              husky # managing git hooks
-              typos # check misspelling
-            ];
+            packages =
+              with pkgs;
+              [
+                just # just a command runner
+                cocogitto # conventional commit toolkit
+                husky # managing git hooks
+                typos # check misspelling
+              ]
+              ++ [
+                pkgs-stable.awscli2 # aws command line tool
+              ];
             # The shell script executed when the environment is activated.
             shellHook = ''
               # install git hook managed by husky
